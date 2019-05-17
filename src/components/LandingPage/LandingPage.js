d9f1429803f2e79f24cb48544e4cbed09518847a
Joe Webb <webbdeveloping@gmail.com>
2019-05-16 18:54:31 -0600

    edit footer

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,6 @@ import Footer from '../Footer/Footer';
 import Downloads from '../Downloads/Downloads';
 import '../../Pages/MakeMoney/MakeMoney.css';
 import AnimatedGrid from '../AnimatedGrid';
-const backgroundImg = require('./alessio.jpg');
 
 export default function LandingPage() {
   const goToLyft = () => {
@@ -15,28 +14,13 @@ export default function LandingPage() {
   return (
     <div style={{ height: '100%' }} className=''>
       <div className='hero-image'>
-        {/* <div className='hero-text'>
-          <div className=''>
-            <Link className='link-button' to='/DriverPage'>
-              <h4>Apply</h4>
-            </Link>
-          </div>
-
-          <div className='ride-container'>
-            <button className='rider-button'>Sign Up To Ride</button>
-          </div>
-        </div> */}
         <div className='col '>
-          <div className=' col icon-box a-box not'>
-            <h1 style={{ color: '#ff00bf' }}>
+          <div className=' col icon-box a-box not shadow-box'>
+            <h1 style={{ color: 'white' }}>
               The Average Ride Share Driver Earns $26 an Hour!
             </h1>
             <h4>Apply Today and Start Earning!!</h4>
-            {/* <Link to='/DriverPage' style={{ textDecoration: 'none' }}>
-              <div className='driver-button'>
-                <h4>Apply to Drive</h4>
-              </div>
-            </Link> */}
+
             <div className='download-button'>
               <a href='/LyftPage'>Apply Now!</a>
             </div>
@@ -44,29 +28,13 @@ export default function LandingPage() {
         </div>
       </div>
       <div className='why-drive'>
-        {/* <div className='ride-container' id='white'>
-          <Link to='/DriverPage' style={{ textDecoration: 'none' }}>
-            <button className='rider-button-b'>Why Drive: Driver</button>
-          </Link>
-        </div>
-        <div className='ride-container' id='white'>
-          <button className='rider-button-b'>Why Ride: Rider</button>
-        </div> */}
         <div className='why-button'>
-          <a
-            className='wd'
-            // style={{ backgroundColor: '#ff00bf' }}
-            href='/DriverPage'
-          >
+          <a className='wd' href='/DriverPage'>
             Why Drive?
           </a>
         </div>
         <div className='why-button'>
-          <a
-            className='wr'
-            // style={{ backgroundColor: '#11111f', color: '#ff00bf' }}
-            href='/RiderPage'
-          >
+          <a className='wr' href='/RiderPage'>
             Why Ride?
           </a>
         </div>
